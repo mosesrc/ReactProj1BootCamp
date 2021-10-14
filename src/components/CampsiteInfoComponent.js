25737d1c00dd0e48c91a4d7808a04595fe5eff23
Ryan Moses <ryanchristophermoses@gmail.com>
2021-10-14 00:51:10 -0400

    Presentational and Container Components

---
+++ src/components/CampsiteInfoComponent.js
@@ -0,0 +1,60 @@
+import { Component } from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class CampsiteInfo extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    renderComments(comments) {
+        if (comments) {
+            return (
+                <div className="col-md-5 m-1">
+                    <h4>Comments</h4>
+                    {this.props.campsite.comments.map(comment => {
+                        return (
+                            <div key={this.props.campsite.comments.id}>
+                                <p> {comment.text} </p>
+                                <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))} </p>
+                            </div>
+                        )
+                    })}
+                </div>
+            );
+        } else {
+            return <div></div>
+        }
+    }
+
+    renderCampsite(campsite) {
+        return (
+            <div className="col-md-5 m-1">
+                <Card>
+                    <CardImg top src={campsite.image} alt={campsite.name} />
+                    <CardBody>
+                        <CardTitle>{campsite.name}</CardTitle>
+                        <CardText>{campsite.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+        )
+    }
+
+    render() {
+        if (this.props.campsite) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
+                    </div>
+                </div>
+            );
+        } else {
+            return <div />;
+        }
+
+    }
+}
+
+export default CampsiteInfo
